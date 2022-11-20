90ae2cd51c1dde5c84c4d250ca3447271cd1fad9
houssem-bouallegui <houssembouallegui4@gmail.com>
2022-11-20 15:54:26 +0100

    Update index.php
    1->5

--- index.php
+++ index.php
@@ -2,16 +2,50 @@
 require 'vendor/autoload.php';
 
 $client = new MongoDB\Client;
-
-$employe = $client->employes;
-$empcol = $employe->employes;
-
-$documentlist = $empcol->find(
-	['nom' => 'Bouallegui']
-	);
-foreach($documentlist as $doc){
-	var_dump($doc);
+$db = $client->employes;
+$empcol = $db->employes;
+///////////////////////////////////////
+echo("<h1>Réponse 1 : </h1>");
+print_r ('<pre>');	
+foreach($db->listCollections() as $col){
+	print_r($col);
 }
+print_r ('</pre>');
+//////////////////////////////////////
+echo("<h1>Réponse 2 : </h1>");
+$cursor = $empcol->find();
+print_r ('<pre>');	
+foreach($cursor as $doc){
+	print_r($doc);}
+print_r ('</pre>');
+//////////////////////////////////////
+echo("<h1>Réponse 3 : </h1>");
+$cursor = $empcol->count();
+print_r($cursor);
+//////////////////////////////////////
+echo("<h1>Réponse 4 : </h1>");
+/*$filtre=[];
+$option=['sort'=> array('_id' => -1),'limit'=>1];
+$cursor = $empcol->find($filtre,$option);
+*/
+$cursor = $empcol->insertMany([
+	['nom' => 'Bouallegui' , 'prenom' => 'Houssem' , 'prime' => '500'],
+	['nom' => 'Anoun' , 'prenom' => 'Mohamed' , 'anciennete' => '10']
+]);
+print_r ('<pre>');	
+printf ("Document inserer %d", $cursor->getInsertedCount());
+var_dump($cursor->getInsertedIds());
+print_r ('</pre>');
+//////////////////////////////////////
+echo("<h1>Réponse 5 : </h1>");
+$cursor = $empcol->find(
+	['prenom': => '/^D.*/']
+);
+print_r ('<pre>');	
+foreach($cursor as $doc){
+	print_r($doc);}
+print_r ('</pre>');
+
 
 
 
