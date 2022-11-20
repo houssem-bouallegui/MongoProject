2c44b94ecf164dfb95988495a1a1d709c9bcfbbc
houssem-bouallegui <houssembouallegui4@gmail.com>
2022-11-20 17:34:25 +0100

    Update index.php
    finished all convertion

--- index.php
+++ index.php
@@ -24,18 +24,18 @@ $cursor = $empcol->count();
 print_r($cursor);
 //////////////////////////////////////
 echo("<h1>Réponse 4 : </h1>");
-/*$filtre=[];
+$filtre=[];
 $option=['sort'=> array('_id' => -1),'limit'=>1];
 $cursor = $empcol->find($filtre,$option);
 
 $cursor = $empcol->insertMany([
-	['nom' => 'Bouallegui' , 'prenom' => 'HoussemB' , 'prime' => '500'],
-	['nom' => 'Anoun' , 'prenom' => 'Mohamed' , 'anciennete' => '10']
+	['nom' => 'Bouallegui' , 'prenom' => 'HoussemB' , 'prime' => 500],
+	['nom' => 'Anoun' , 'prenom' => 'Mohamed' , 'anciennete' => 10]
 ]);
 print_r ('<pre>');	
 printf ("Document inserer %d", $cursor->getInsertedCount());
 var_dump($cursor->getInsertedIds());
-print_r ('</pre>');*/
+print_r ('</pre>');
 //////////////////////////////////////
 echo("<h1>Réponse 5 : </h1>");
 $regex = new MongoDB\BSON\Regex("^D.*", "i");
@@ -75,10 +75,52 @@ print_r ('<pre>');
 foreach($cursor as $doc){
 	print_r($doc);}
 print_r ('</pre>');
-
-
-
-
+//////////////////////////////////////
+echo("<h1>Réponse 9 : </h1>");
+$filter	= ['adresse.rue' => ['$exists' => true] ];
+$option=['projection' => ['nom' => 1 , 'adresse' => 1]];
+$cursor = $empcol->find($filter,$option);
+print_r ('<pre>');	
+foreach($cursor as $doc){
+	print_r($doc);}
+print_r ('</pre>');
+//////////////////////////////////////
+echo("<h1>Réponse 10 : </h1>");
+$filter	= ['prime' => ['$exists' => true] ];
+$option=['$inc' => ['prime' => 200]];
+$cursor = $empcol->updateMany($filter,$option);
+print_r ('<pre>');	
+printf ('Success + 200');
+print_r ('</pre>');
+//////////////////////////////////////
+echo("<h1>Réponse 11 : </h1>");
+$filter	= ['anciennete' => ['$exists' => true] ];
+$option=['sort' => ['anciennete' => -1] , 'limit' => 10];
+$cursor = $empcol->find($filter,$option);
+print_r ('<pre>');	
+foreach($cursor as $doc){
+	print_r($doc);}
+print_r ('</pre>');
+//////////////////////////////////////
+echo("<h1>Réponse 12 : </h1>");
+$filter	= ['adresse.ville' => 'Toulouse' ];
+$option=['projection' => ['nom' => 1 , 'prenom' => 1 , 'adresse.ville' => 1]];
+$cursor = $empcol->find($filter,$option);
+print_r ('<pre>');	
+foreach($cursor as $doc){
+	print_r($doc);}
+print_r ('</pre>');
+//////////////////////////////////////
+echo("<h1>Réponse 13 : </h1>");
+$regex = new MongoDB\BSON\Regex("^M.*", "i");
+$filter	= ['$and' => [['prenom' => $regex ] , ['$or' => [['adresse.ville' => 'Paris'] , ['adresse.ville' => 'Bordeaux']]]]];
+$option=['projection' => ['nom' => 1 , 'prenom' => 1 , 'adresse.ville' => 1]];
+$cursor = $empcol->find($filter,$option);
+print_r ('<pre>');	
+foreach($cursor as $doc){
+	print_r($doc);
+	}
+print_r ('</pre>');
 
 
 
