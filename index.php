2db4a09933969025f47034730cc531bb3fa776e3
houssem-bouallegui <houssembouallegui4@gmail.com>
2023-01-15 16:24:05 +0100

    final update
    Fixed everything

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <?php
 require 'vendor/autoload.php';
-
+echo '<html><head>';
+echo '<link rel="stylesheet" type="text/css" href="styles.css">';
+echo '</head><body>';
 $client = new MongoDB\Client;
 $db = $client->employes;
 $empcol = $db->employes;
@@ -11,13 +13,78 @@ foreach($db->listCollections() as $col){
 	print_r($col);
 }
 print_r ('</pre>');
+
+
 //////////////////////////////////////
 echo("<h1>Réponse 2 : </h1>");
 $cursor = $empcol->find();
 print_r ('<pre>');	
+echo "<table>";
+echo "<tr>";
+echo "<th>nom</th>";
+echo "<th>prenom</th>";
+echo "<th>anciennete</th>";
+echo "<th>numero</th>";
+echo "<th>codepostal</th>";
+echo "<th>ville</th>";
+echo "<th>tel</th>";
+echo "</tr>";
 foreach($cursor as $doc){
-	print_r($doc);}
-print_r ('</pre>');
+	//print_r($doc);
+//print_r ('</pre>');
+
+
+
+
+    echo "<tr>";
+    if (isset($doc["nom"])){
+	echo "<td>" . $doc["nom"] . "</td>";
+		}
+	 else{
+		echo "<td> n'as pas de nom </td>";
+		}
+    
+	if (isset($doc["prenom"])){
+	echo "<td>" . $doc["prenom"] . "</td>";
+		}
+	 else{
+		echo "<td> n'as pas de prenom </td>";
+		}
+	 if (isset($doc["anciennete"])){
+	echo "<td>" . $doc["anciennete"] . "</td>";
+		}
+	 else{
+		echo "<td> n'as pas d'anciennete </td>";
+		}
+    if (isset($doc["adresse"]["numero"])){
+		echo "<td>" . $doc["adresse"]["numero"] . "</td>";
+		}
+	else{
+		echo "<td> n'as pas d'adresss </td>";
+		}
+	if (isset($doc["adresse"])){
+		echo "<td>" . $doc["adresse"]["codepostal"] . "</td>";
+		}
+	else{
+		echo "<td> n'as pas d'adresse </td>";
+		}
+    if (isset($doc["adresse"])){
+		echo "<td>" . $doc["adresse"]["ville"] . "</td>";
+		}
+	else{
+		echo "<td> n'as pas d'adresss </td>";
+		}
+	if (isset($doc["tel"])){
+		echo "<td>" . $doc["tel"] . "</td>";
+		}
+	else{
+		echo "<td> n'as pas de numero telephone </td>";
+		}
+	echo "</tr>";
+
+}
+echo "</table>";
+
 //////////////////////////////////////
 echo("<h1>Réponse 3 : </h1>");
 $cursor = $empcol->count();
@@ -43,47 +110,353 @@ $cursor = $empcol->find(
 	['prenom' => $regex]
 );
 print_r ('<pre>');	
-foreach($cursor as $doc){
+echo "<table>";
+echo "<tr>";
+echo "<th>nom</th>";
+echo "<th>prenom</th>";
+echo "<th>anciennete</th>";
+echo "<th>numero</th>";
+echo "<th>codepostal</th>";
+echo "<th>ville</th>";
+echo "<th>tel</th>";
+echo "</tr>";
+foreach($cursor as $doc){/*
 	print_r($doc);}
-print_r ('</pre>');
+print_r ('</pre>');*/
+
+
+
+    echo "<tr>";
+    if (isset($doc["nom"])){
+	echo "<td>" . $doc["nom"] . "</td>";
+		}
+	 else{
+		echo "<td> n'as pas de nom </td>";
+		}
+    
+	if (isset($doc["prenom"])){
+	echo "<td>" . $doc["prenom"] . "</td>";
+		}
+	 else{
+		echo "<td> n'as pas de prenom </td>";
+		}
+	 if (isset($doc["anciennete"])){
+	echo "<td>" . $doc["anciennete"] . "</td>";
+		}
+	 else{
+		echo "<td> n'as pas d'anciennete </td>";
+		}
+    if (isset($doc["adresse"])){
+		echo "<td>" . $doc["adresse"]["numero"] . "</td>";
+		}
+	else{
+		echo "<td> n'as pas d'adresss </td>";
+		}
+	if (isset($doc["adresse"])){
+		echo "<td>" . $doc["adresse"]["codepostal"] . "</td>";
+		}
+	else{
+		echo "<td> n'as pas d'adresse </td>";
+		}
+    if (isset($doc["adresse"])){
+		echo "<td>" . $doc["adresse"]["ville"] . "</td>";
+		}
+	else{
+		echo "<td> n'as pas d'adresss </td>";
+		}
+	if (isset($doc["tel"])){
+		echo "<td>" . $doc["tel"] . "</td>";
+		}
+	else{
+		echo "<td> n'as pas de numero telephone </td>";
+		}
+	echo "</tr>";
+
+}
+echo "</table>";
+
 //////////////////////////////////////
 echo("<h1>Réponse 6 : </h1>");
 $regex = new MongoDB\BSON\Regex("^D.*|.*D$", "i");
 $cursor = $empcol->find(
 	['prenom' => $regex]
 );
-print_r ('<pre>');	
-foreach($cursor as $doc){
+//print_r ('<pre>');	
+echo "<table>";
+echo "<tr>";
+echo "<th>nom</th>";
+echo "<th>prenom</th>";
+echo "<th>anciennete</th>";
+echo "<th>numero</th>";
+echo "<th>codepostal</th>";
+echo "<th>ville</th>";
+echo "<th>tel</th>";
+echo "</tr>";
+foreach($cursor as $doc){/*
 	print_r($doc);}
-print_r ('</pre>');
+print_r ('</pre>');*/
+
+
+
+    echo "<tr>";
+    if (isset($doc["nom"])){
+	echo "<td>" . $doc["nom"] . "</td>";
+		}
+	 else{
+		echo "<td> n'as pas de nom </td>";
+		}
+    
+	if (isset($doc["prenom"])){
+	echo "<td>" . $doc["prenom"] . "</td>";
+		}
+	 else{
+		echo "<td> n'as pas de prenom </td>";
+		}
+	 if (isset($doc["anciennete"])){
+	echo "<td>" . $doc["anciennete"] . "</td>";
+		}
+	 else{
+		echo "<td> n'as pas d'anciennete </td>";
+		}
+    if (isset($doc["adresse"])){
+		echo "<td>" . $doc["adresse"]["numero"] . "</td>";
+		}
+	else{
+		echo "<td> n'as pas d'adresss </td>";
+		}
+	if (isset($doc["adresse"])){
+		echo "<td>" . $doc["adresse"]["codepostal"] . "</td>";
+		}
+	else{
+		echo "<td> n'as pas d'adresse </td>";
+		}
+    if (isset($doc["adresse"])){
+		echo "<td>" . $doc["adresse"]["ville"] . "</td>";
+		}
+	else{
+		echo "<td> n'as pas d'adresss </td>";
+		}
+	if (isset($doc["tel"])){
+		echo "<td>" . $doc["tel"] . "</td>";
+		}
+	else{
+		echo "<td> n'as pas de numero telephone </td>";
+		}
+	echo "</tr>";
+
+}
+echo "</table>";
 //////////////////////////////////////
 echo("<h1>Réponse 7 : </h1>");
 $regex = new MongoDB\BSON\Regex("^D[a-z]{5}$", "i");
 $cursor = $empcol->find(
 	['prenom' => $regex]
 );
-print_r ('<pre>');	
-foreach($cursor as $doc){
+//print_r ('<pre>');	
+echo "<table>";
+echo "<tr>";
+echo "<th>nom</th>";
+echo "<th>prenom</th>";
+echo "<th>anciennete</th>";
+echo "<th>numero</th>";
+echo "<th>codepostal</th>";
+echo "<th>ville</th>";
+echo "<th>tel</th>";
+echo "</tr>";
+foreach($cursor as $doc){/*
 	print_r($doc);}
-print_r ('</pre>');
+print_r ('</pre>');*/
+
+
+
+    echo "<tr>";
+    if (isset($doc["nom"])){
+	echo "<td>" . $doc["nom"] . "</td>";
+		}
+	 else{
+		echo "<td> n'as pas de nom </td>";
+		}
+    
+	if (isset($doc["prenom"])){
+	echo "<td>" . $doc["prenom"] . "</td>";
+		}
+	 else{
+		echo "<td> n'as pas de prenom </td>";
+		}
+	 if (isset($doc["anciennete"])){
+	echo "<td>" . $doc["anciennete"] . "</td>";
+		}
+	 else{
+		echo "<td> n'as pas d'anciennete </td>";
+		}
+    if (isset($doc["adresse"])){
+		echo "<td>" . $doc["adresse"]["numero"] . "</td>";
+		}
+	else{
+		echo "<td> n'as pas d'adresss </td>";
+		}
+	if (isset($doc["adresse"])){
+		echo "<td>" . $doc["adresse"]["codepostal"] . "</td>";
+		}
+	else{
+		echo "<td> n'as pas d'adresse </td>";
+		}
+    if (isset($doc["adresse"])){
+		echo "<td>" . $doc["adresse"]["ville"] . "</td>";
+		}
+	else{
+		echo "<td> n'as pas d'adresss </td>";
+		}
+	if (isset($doc["tel"])){
+		echo "<td>" . $doc["tel"] . "</td>";
+		}
+	else{
+		echo "<td> n'as pas de numero telephone </td>";
+		}
+	echo "</tr>";
+
+}
+echo "</table>";
 //////////////////////////////////////
 echo("<h1>Réponse 8 : </h1>");
 $filter	= ['anciennete' => ['$gte' => 10] ];
 $option=[];
 $cursor = $empcol->find($filter,$option);
-print_r ('<pre>');	
-foreach($cursor as $doc){
+//print_r ('<pre>');	
+echo "<table>";
+echo "<tr>";
+echo "<th>nom</th>";
+echo "<th>prenom</th>";
+echo "<th>anciennete</th>";
+echo "<th>numero</th>";
+echo "<th>codepostal</th>";
+echo "<th>ville</th>";
+echo "<th>tel</th>";
+echo "</tr>";
+
+foreach($cursor as $doc){/*
 	print_r($doc);}
-print_r ('</pre>');
+print_r ('</pre>');*/
+
+
+    echo "<tr>";
+    if (isset($doc["nom"])){
+	echo "<td>" . $doc["nom"] . "</td>";
+		}
+	 else{
+		echo "<td> n'as pas de nom </td>";
+		}
+    
+	if (isset($doc["prenom"])){
+	echo "<td>" . $doc["prenom"] . "</td>";
+		}
+	 else{
+		echo "<td> n'as pas de prenom </td>";
+		}
+	 if (isset($doc["anciennete"])){
+	echo "<td>" . $doc["anciennete"] . "</td>";
+		}
+	 else{
+		echo "<td> n'as pas d'anciennete </td>";
+		}
+    if (isset($doc["adresse"]["numero"])){
+		echo "<td>" . $doc["adresse"]["numero"] . "</td>";
+		}
+	else{
+		echo "<td> n'as pas d'adresss </td>";
+		}
+	if (isset($doc["adresse"])){
+		echo "<td>" . $doc["adresse"]["codepostal"] . "</td>";
+		}
+	else{
+		echo "<td> n'as pas d'adresse </td>";
+		}
+    if (isset($doc["adresse"])){
+		echo "<td>" . $doc["adresse"]["ville"] . "</td>";
+		}
+	else{
+		echo "<td> n'as pas d'adresss </td>";
+		}
+	if (isset($doc["tel"])){
+		echo "<td>" . $doc["tel"] . "</td>";
+		}
+	else{
+		echo "<td> n'as pas de numero telephone </td>";
+		}
+	echo "</tr>";
+
+}
+echo "</table>";
 //////////////////////////////////////
 echo("<h1>Réponse 9 : </h1>");
 $filter	= ['adresse.rue' => ['$exists' => true] ];
 $option=['projection' => ['nom' => 1 , 'adresse' => 1]];
 $cursor = $empcol->find($filter,$option);
-print_r ('<pre>');	
-foreach($cursor as $doc){
+//print_r ('<pre>');	
+echo "<table>";
+echo "<tr>";
+echo "<th>nom</th>";
+echo "<th>prenom</th>";
+echo "<th>anciennete</th>";
+echo "<th>numero</th>";
+echo "<th>codepostal</th>";
+echo "<th>ville</th>";
+echo "<th>tel</th>";
+echo "</tr>";
+foreach($cursor as $doc){/*
 	print_r($doc);}
-print_r ('</pre>');
+print_r ('</pre>');*/
+
+
+
+    echo "<tr>";
+    if (isset($doc["nom"])){
+	echo "<td>" . $doc["nom"] . "</td>";
+		}
+	 else{
+		echo "<td> n'as pas de nom </td>";
+		}
+    
+	if (isset($doc["prenom"])){
+	echo "<td>" . $doc["prenom"] . "</td>";
+		}
+	 else{
+		echo "<td> n'as pas de prenom </td>";
+		}
+	 if (isset($doc["anciennete"])){
+	echo "<td>" . $doc["anciennete"] . "</td>";
+		}
+	 else{
+		echo "<td> n'as pas d'anciennete </td>";
+		}
+    if (isset($doc["adresse"])){
+		echo "<td>" . $doc["adresse"]["numero"] . "</td>";
+		}
+	else{
+		echo "<td> n'as pas d'adresss </td>";
+		}
+	if (isset($doc["adresse"])){
+		echo "<td>" . $doc["adresse"]["codepostal"] . "</td>";
+		}
+	else{
+		echo "<td> n'as pas d'adresse </td>";
+		}
+    if (isset($doc["adresse"])){
+		echo "<td>" . $doc["adresse"]["ville"] . "</td>";
+		}
+	else{
+		echo "<td> n'as pas d'adresss </td>";
+		}
+	if (isset($doc["tel"])){
+		echo "<td>" . $doc["tel"] . "</td>";
+		}
+	else{
+		echo "<td> n'as pas de numero telephone </td>";
+		}
+	echo "</tr>";
+
+}
+echo "</table>";
 //////////////////////////////////////
 echo("<h1>Réponse 10 : </h1>");
 $filter	= ['prime' => ['$exists' => true] ];
@@ -97,31 +470,214 @@ echo("<h1>Réponse 11 : </h1>");
 $filter	= ['anciennete' => ['$exists' => true] ];
 $option=['sort' => ['anciennete' => -1] , 'limit' => 10];
 $cursor = $empcol->find($filter,$option);
-print_r ('<pre>');	
-foreach($cursor as $doc){
+//print_r ('<pre>');	
+echo "<table>";
+echo "<tr>";
+echo "<th>nom</th>";
+echo "<th>prenom</th>";
+echo "<th>anciennete</th>";
+echo "<th>numero</th>";
+echo "<th>codepostal</th>";
+echo "<th>ville</th>";
+echo "<th>tel</th>";
+echo "</tr>";
+foreach($cursor as $doc){/*
 	print_r($doc);}
-print_r ('</pre>');
+print_r ('</pre>');*/
+
+
+
+    echo "<tr>";
+    if (isset($doc["nom"])){
+	echo "<td>" . $doc["nom"] . "</td>";
+		}
+	 else{
+		echo "<td> n'as pas de nom </td>";
+		}
+    
+	if (isset($doc["prenom"])){
+	echo "<td>" . $doc["prenom"] . "</td>";
+		}
+	 else{
+		echo "<td> n'as pas de prenom </td>";
+		}
+	 if (isset($doc["anciennete"])){
+	echo "<td>" . $doc["anciennete"] . "</td>";
+		}
+	 else{
+		echo "<td> n'as pas d'anciennete </td>";
+		}
+    if (isset($doc["adresse"])){
+		echo "<td>" . $doc["adresse"]["numero"] . "</td>";
+		}
+	else{
+		echo "<td> n'as pas d'adresss </td>";
+		}
+	if (isset($doc["adresse"])){
+		echo "<td>" . $doc["adresse"]["codepostal"] . "</td>";
+		}
+	else{
+		echo "<td> n'as pas d'adresse </td>";
+		}
+    if (isset($doc["adresse"])){
+		echo "<td>" . $doc["adresse"]["ville"] . "</td>";
+		}
+	else{
+		echo "<td> n'as pas d'adresss </td>";
+		}
+	if (isset($doc["tel"])){
+		echo "<td>" . $doc["tel"] . "</td>";
+		}
+	else{
+		echo "<td> n'as pas de numero telephone </td>";
+		}
+	echo "</tr>";
+
+}
+echo "</table>";
 //////////////////////////////////////
 echo("<h1>Réponse 12 : </h1>");
 $filter	= ['adresse.ville' => 'Toulouse' ];
 $option=['projection' => ['nom' => 1 , 'prenom' => 1 , 'adresse.ville' => 1]];
 $cursor = $empcol->find($filter,$option);
-print_r ('<pre>');	
-foreach($cursor as $doc){
+//print_r ('<pre>');	
+echo "<table>";
+echo "<tr>";
+echo "<th>nom</th>";
+echo "<th>prenom</th>";
+echo "<th>anciennete</th>";
+echo "<th>numero</th>";
+echo "<th>codepostal</th>";
+echo "<th>ville</th>";
+echo "<th>tel</th>";
+echo "</tr>";
+
+foreach($cursor as $doc){/*
 	print_r($doc);}
-print_r ('</pre>');
+print_r ('</pre>');*/
+
+
+    echo "<tr>";
+    if (isset($doc["nom"])){
+	echo "<td>" . $doc["nom"] . "</td>";
+		}
+	 else{
+		echo "<td> n'as pas de nom </td>";
+		}
+    
+	if (isset($doc["prenom"])){
+	echo "<td>" . $doc["prenom"] . "</td>";
+		}
+	 else{
+		echo "<td> n'as pas de prenom </td>";
+		}
+	 if (isset($doc["anciennete"])){
+	echo "<td>" . $doc["anciennete"] . "</td>";
+		}
+	 else{
+		echo "<td> n'as pas d'anciennete </td>";
+		}
+    if (isset($doc["adresse"]["numero"])){
+		echo "<td>" . $doc["adresse"]["numero"] . "</td>";
+		}
+	else{
+		echo "<td> n'as pas d'adresss </td>";
+		}
+	if (isset($doc["adresse"]["codepostal"])){
+		echo "<td>" . $doc["adresse"]["codepostal"] . "</td>";
+		}
+	else{
+		echo "<td> n'as pas d'adresse </td>";
+		}
+    if (isset($doc["adresse"]["ville"])){
+		echo "<td>" . $doc["adresse"]["ville"] . "</td>";
+		}
+	else{
+		echo "<td> n'as pas d'adresss </td>";
+		}
+	if (isset($doc["tel"])){
+		echo "<td>" . $doc["tel"] . "</td>";
+		}
+	else{
+		echo "<td> n'as pas de numero telephone </td>";
+		}
+	echo "</tr>";
+}
+
+echo "</table>";
 //////////////////////////////////////
 echo("<h1>Réponse 13 : </h1>");
 $regex = new MongoDB\BSON\Regex("^M.*", "i");
 $filter	= ['$and' => [['prenom' => $regex ] , ['$or' => [['adresse.ville' => 'Paris'] , ['adresse.ville' => 'Bordeaux']]]]];
 $option=['projection' => ['nom' => 1 , 'prenom' => 1 , 'adresse.ville' => 1]];
 $cursor = $empcol->find($filter,$option);
-print_r ('<pre>');	
-foreach($cursor as $doc){
+//print_r ('<pre>');	
+echo "<table>";
+echo "<tr>";
+echo "<th>nom</th>";
+echo "<th>prenom</th>";
+echo "<th>anciennete</th>";
+echo "<th>numero</th>";
+echo "<th>codepostal</th>";
+echo "<th>ville</th>";
+echo "<th>tel</th>";
+echo "</tr>";
+
+foreach($cursor as $doc){/*
 	print_r($doc);
 	}
-print_r ('</pre>');
+print_r ('</pre>');*/
 
 
+    echo "<tr>";
+    if (isset($doc["nom"])){
+	echo "<td>" . $doc["nom"] . "</td>";
+		}
+	 else{
+		echo "<td> n'as pas de nom </td>";
+		}
+    
+	if (isset($doc["prenom"])){
+	echo "<td>" . $doc["prenom"] . "</td>";
+		}
+	 else{
+		echo "<td> n'as pas de prenom </td>";
+		}
+	 if (isset($doc["anciennete"])){
+	echo "<td>" . $doc["anciennete"] . "</td>";
+		}
+	 else{
+		echo "<td> n'as pas d'anciennete </td>";
+		}
+    if (isset($doc["adresse"]["numero"])){
+		echo "<td>" . $doc["adresse"]["numero"] . "</td>";
+		}
+	else{
+		echo "<td> n'as pas d'adresss </td>";
+		}
+	if (isset($doc["adresse"]["codepostal"])){
+		echo "<td>" . $doc["adresse"]["codepostal"] . "</td>";
+		}
+	else{
+		echo "<td> n'as pas d'adresse </td>";
+		}
+    if (isset($doc["adresse"]["ville"])){
+		echo "<td>" . $doc["adresse"]["ville"] . "</td>";
+		}
+	else{
+		echo "<td> n'as pas d'adresss </td>";
+		}
+	if (isset($doc["tel"])){
+		echo "<td>" . $doc["tel"] . "</td>";
+		}
+	else{
+		echo "<td> n'as pas de numero telephone </td>";
+		}
+	echo "</tr>";
+}
 
+echo "</table>";
+
+
+echo '</body></html>'
 ?>
